e049f0d560be23e2991dd773490dd014e9a8f292
Claudio Omar Melendrez Baeza <claudio.melendrez@gmail.com>
2017-03-12 11:40:47 -0300

    Add basic admin config

--- apps/currencies/admin.py
+++ apps/currencies/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+"""
+Admin views configuration for currencies application.
+"""
+from django.contrib.admin import site, ModelAdmin
 
-# Register your models here.
+from .models import Currency, ExchangeRate
+
+
+class CurrencyAdmin(ModelAdmin):
+    """
+    Admin for Currency model.
+    """
+    list_display = ('id', 'code', 'symbol', 'name',)
+
+
+class ExchangeRateAdmin(ModelAdmin):
+    """
+    Admin for ExchangeRate model.
+    """
+    list_display = ('bought_currency', 'sold_currency', 'value')
+
+
+site.register(Currency, CurrencyAdmin)
+site.register(ExchangeRate, ExchangeRateAdmin)
--- apps/currencies/models.py
+++ apps/currencies/models.py
@@ -8,6 +8,9 @@ class Currency(Model):
     """
     Currency object.
     """
+    class Meta(object):
+        verbose_name_plural = 'currencies'
+
     # Database fields
     name = CharField(
         max_length=100,
@@ -21,6 +24,9 @@ class Currency(Model):
         max_length=3,
         help_text='Short-hand symbol.'
     )
+
+    def __str__(self):
+        return self.code
 
 
 class ExchangeRate(Model):
@@ -41,3 +47,6 @@ class ExchangeRate(Model):
         max_digits=10,
         help_text="Bought units for one sold unit."
     )
+
+    def __str__(self):
+        return '{}-{}'.format(self.bought_currency, self.sold_currency)
